a198515ed2e43e98120e6dd2c711b95cd5513828
SurajLodh <surajlodh10962@gmail.com>
2021-06-20 21:46:44 +0530

    fiter

--- FOOD/urls.py
+++ FOOD/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('create', views.Add_new, name='add_new'),
     path('alldata', views.View_All, name='all_data'),
+    path('searchdata', views.Search_data, name='searchdata'),
 
 ]
